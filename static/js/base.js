c4a9674ca50016484e33409ede8f353e7b5aff69
luogeger <xiaoqing1232@163.com>
2018-02-05 13:45:11 +0800

    大菜单bug修复，没有动效

--- static/js/base.js
+++ static/js/base.js
@@ -915,12 +915,16 @@ var _sidle = function () {
 
         init: function (opt) {
             // 初始化dom元素
+            var $content   = $('.content-item')// .content-item
             var $container = $('#navSidle');
             $container.html(this.render(opt))
-            this.$a         = $container.find('a');// 所有a标签
             this.$btn       = $container.find('#collapseBtn');// 三明治按钮
+            this.$a         = $container.find('a');// 所有a标签
+
+
 
             // 初始化事件
+            $content.css({"min-height": $(window).height() -60 +"px"});// 设置.content-item的最小高度
             this.bind()
         },// init
 
@@ -963,6 +967,7 @@ var _sidle = function () {
                 html += '</div>';// .panel
             });// menu.forEach
             html +=  '</div>';
+
             return html;
         },// render
 
@@ -972,7 +977,7 @@ var _sidle = function () {
                 // 显示默认菜单
                 if (_a.hasClass('active')) {
                     _a.parents('.panel')
-                        .children('ul').slideDown(200)// 二级菜单显示
+                        .children('ul').css('display', 'block')// 二级菜单显示
                         .siblings('a').addClass('flag-open active-parent')// a标签字体颜色变主题色
                         .children('i:last-child').css('transform', 'rotate(180deg)')// 箭头朝下
                 }// if
@@ -1057,7 +1062,7 @@ var _sidle = function () {
                 var open = !$("#collapseBtn").hasClass("to-open");// true出去打开状态
                 if(open){
                     $('#navContent').css({'padding-left': '60px'});
-                    $('#navSidle').css('left', '0px').addClass('i-sidle-slim');
+                    $('#navSidle').addClass('i-sidle-slim');
                     $('#navHead').addClass('to-open')
                     $('#collapseBtn').addClass('to-open')
                     $('#navSidle .panel').each(function (index, item) {
@@ -1067,11 +1072,10 @@ var _sidle = function () {
                     })
                 }else{
                     $('#navContent').css({'padding-left': '180px'});
-                    $('#navSidle').css('left', '0px').removeClass('i-sidle-slim')
+                    $('#navSidle').removeClass('i-sidle-slim')
                     $('#navHead').removeClass('to-open')
                     $('#collapseBtn').removeClass('to-open')
                     $('#navSidle .panel').each(function (index, item) {
-                        // $(item).children('ul').css('display', 'none')
                         if ($(item).children('a').hasClass('active-parent')) {
                             $(item).children('a').addClass('flag-open')
                             $(item).children('ul').slideDown(200)
@@ -1080,13 +1084,15 @@ var _sidle = function () {
 
                 }
             })
+
+
         },// bind
 
     }// prototype
 
+
     $.fn.Slide = function (options) {
         return new Sidle(options)
-
     }// $.fn
 }($, window);// end
 
